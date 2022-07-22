bc9f0e494293ca0af221fbe1863aa2c8b4730d17
Michael Henry <michaeljhenry@hotmail.ca>
2022-07-22 14:15:43 -0400

    sending our first (test) email

--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -1,6 +1,32 @@
+import AWS from 'aws-sdk';
+
+const ses = new AWS.SES({ region: 'us-east-2' });
+
 async function sendMail(event, context) {
-  console.log(sendMail);
-  return event;
+  const params = {
+    Source: 'michaeljhenry@hotmail.ca',
+    Destination: {
+      ToAddresses: ['michaeljhenry02@gmail.com']
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: 'Hello from Cha1nman'
+        }
+      },
+      Subject: {
+        Data: 'Test Mail'
+      }
+    },
+  };
+
+  try {
+    const result = await ses.sendEmail(params).promise();
+    console.log(result);
+    return result;
+  } catch(error) {
+    console.error(error);
+  }
 }
 
 export const handler = sendMail;
