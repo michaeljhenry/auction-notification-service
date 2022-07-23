731cc5d2ccebc7b9d998f4f757d1b46a90092465
Michael Henry <michaeljhenry@hotmail.ca>
2022-07-23 00:23:07 -0400

    provisioning AWS SQS Message Queue (MailQueue) and manually sending SQS messages

--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -3,19 +3,25 @@ import AWS from 'aws-sdk';
 const ses = new AWS.SES({ region: 'us-east-2' });
 
 async function sendMail(event, context) {
+  const record = event.Records[0];
+  console.log('record processing', record);
+
+  const email = JSON.parse(record.body);
+
+  const { subject, body, recipient } = email;
   const params = {
     Source: 'michaeljhenry@hotmail.ca',
     Destination: {
-      ToAddresses: ['michaeljhenry02@gmail.com']
+      ToAddresses: [recipient]
     },
     Message: {
       Body: {
         Text: {
-          Data: 'Hello from Cha1nman'
+          Data: body
         }
       },
       Subject: {
-        Data: 'Test Mail'
+        Data: subject
       }
     },
   };
